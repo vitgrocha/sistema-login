9e1aedd479959500c11d6a3fbb7606d20d1364de
Vitoria Gabriella Rocha <90655197+vitgrocha@users.noreply.github.com>
2023-04-19 18:00:23 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import RoutesApp from "./routes";
+import GlobalStyle from "./styles/global";
+
+const App = () => {
+    return (
+        <>
+            <RoutesApp></RoutesApp>
+            <GlobalStyle></GlobalStyle>
+        </>
+    )
+}
+
+export default App

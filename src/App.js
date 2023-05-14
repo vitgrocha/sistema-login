436b56adf76aeb91a8f01ba34367be4816a60c49
Vitoria Gabriella Rocha <90655197+vitgrocha@users.noreply.github.com>
2023-05-14 17:36:45 -0300

    auth include App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { AuthProvider } from "./contexts/auth";
 import RoutesApp from "./routes";
 import GlobalStyle from "./styles/global";
 
 const App = () => {
     return (
-        <>
+        <AuthProvider>
             <RoutesApp></RoutesApp>
             <GlobalStyle></GlobalStyle>
-        </>
+        </AuthProvider>
     )
 }
 
